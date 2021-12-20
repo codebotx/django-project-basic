7f3b898d565076151dbbeb0582d5fef7b59ef641
Anubhab Patnaik <anubhabr50@gmail.com>
2021-12-20 20:28:59 +0530

    basic wesbite ready

--- new/new/urls.py
+++ new/new/urls.py
@@ -1,7 +1,7 @@
-"""new URL Configuration
+"""Hello URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+admin.site.site_header = "Harry Ice Cream Admin"
+admin.site.site_title = "Harry Ice Cream Admin Portal"
+admin.site.index_title = "Welcome to Harry Ice Creams"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+
 ]
